f54860a1dc1415b51b176dd0af92a8c01c6f0b7c
rishabh jha <rishabhjha0@gmail.com>
2021-12-07 12:06:20 +0530

    add a controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home=function(ereq,res){
+    return res.end('<h1>express is running for connectRJ</h1>')
+}
+// module.exports.about=function(req,res){
+//     return res.end('<h1>about</h1>')
+// }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const app=express();
 const port=8000;
+
+//using express router
+app.use('/',require('./routes'));
 app.listen(port,function(err){
     if(err){
         // console.log("error is ",err);
