f8052903a9cc06888b40b41d1b70c46f178f59fa
rishabh jha <rishabhjha0@gmail.com>
2022-02-27 11:30:33 +0530

    chatting engine

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ const chatServer = require('http').Server(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
+//CHANGE
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declarat
 
 
 
